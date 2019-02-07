a1c54c864254294c02af0a1c1a9e72b1a4661e87
Rohan2596 <rohankadam662>
2019-02-07 17:20:42 +0530

    time module file

---
+++ timemod.py
@@ -0,0 +1,8 @@
+import time
+ticks=time.time()#ticks of time
+print("Number of ticks since ",ticks)
+localtime=time.localtime(time.time())#localtime
+print("###local current time:",localtime)
+localtimeasc=time.asctime(time.localtime(time.time()))
+print("####local curent time ASC :",localtimeasc)#local time asc format
+
