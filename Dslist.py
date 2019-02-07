a09d479d69892b83c084200a8c36b1c5f435e751
Rohan2596 <rohankadam662>
2019-02-07 16:19:16 +0530

    Data structure list

---
+++ Dslist.py
@@ -0,0 +1,17 @@
+list1 =['physics','chemistry',1997,2000]
+list2=[1,2,3,4,5]
+list3=["a","b","c","d"]
+print("Acessing the values from lists")#acessing the values from list
+print("list1[0]: ",list1[0])
+print("list2[1:5]:",list2[1:5])
+print("Updating the ")
+print(list1[2])
+list1[2] = 2001
+print("new value aviable at index 2:")
+print(list1)
+print(len(list1))
+print(list1 + list2)
+print(list1*2)
+print( 1997 in list1)
+for x in list2:
+	print(x)
