3120b424c1c0600907a85bab2a0f5e451bc3e186
Rohan2596 <rohankadam662>
2019-02-13 13:17:31 +0530

    python

--- FunctionalPrograms/tictoegame.py
+++ FunctionalPrograms/tictoegame.py
@@ -8,4 +8,7 @@ else:
 	print("Invalid")
 x=[[tic for i in range(3)]for j in range(3)]
 print(x)
- 
+i=int(input("enterthe value of i"))
+j=int(input("enter the value of j"))
+def input_2d(i,j):
+    x[][]
---
+++ FunctionalPrograms/vendmac.py
@@ -0,0 +1,21 @@
+notes=int(input("Enter the value"))
+if notes==1:
+	print("A penny")
+elif notes==2:
+	print("2=1+1")
+elif notes==5:
+	print("2+2+1")
+elif notes==10:
+	print("5*2")
+elif notes==20:
+	print("10*2")
+elif notes==50:
+	print("20*2+10*1")
+elif notes==100:
+	print("50*2")
+elif notes==500:
+	print("100*5")
+elif notes==1000:
+	print("500*2")
+else:
+	print("innalid")
