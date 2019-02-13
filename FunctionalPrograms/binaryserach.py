5ed2027abd2bd5ed1b7349d98cd4977da223b6b5
Rohan2596 <rohankadam662>
2019-02-13 16:54:19 +0530

    python

--- FunctionalPrograms/binaryserach.py
+++ FunctionalPrograms/binaryserach.py
@@ -1,20 +1,26 @@
-# def bubblesort(alist):
-#     for i in range(len(alist)-1,0,-1):
-#         for j in range(i):
-#             if alist[j]> alist[j+1]:
-#                 temp =alist[j]
-#                 alist[j]=alist[j+1]
-#                 alist[j+1]=temp
-#     return alist
-# # print(bubblesort([5,5,1,4,2,10]))
-# def insertionsort(alist):
-#     for i in range(1,len(alist)):
-#         current =alist[i]
-#         while i>0 and alist[i-1]>current:
-#             alist[i]=alist[i-1]
-#             i=i-1
-#             alist[i]=current
-#         print(alist)
-#     return alist
-# print(insertionsort([5,2,3,6,1,8]))
-
+def bubblesort(alist):
+    for i in range(len(alist)-1,0,-1):
+        for j in range(i):
+            if alist[j]> alist[j+1]:
+                temp =alist[j]
+                alist[j]=alist[j+1]
+                alist[j+1]=temp
+    return alist
+    
+alist=bubblesort(['f','e','d','c','b','a']) 
+def binary_sort(sorted_list, key,length,*args):
+    start = 0
+    end = length-1
+    print(length)
+    while start <= end:
+        mid = int((start + end)/2)
+        if key == sorted_list[mid]:
+            print("\nEntered number %d is present at position: %d" % (key, mid))
+            return -1
+        elif key < sorted_list[mid]:
+            end = mid - 1
+        elif key > sorted_list[mid]:
+            start = mid + 1
+    print("\nElement not found!")
+    return -1
+binary_sort(alist,'c',6,2) 
--- FunctionalPrograms/merge_sort.py
+++ FunctionalPrograms/merge_sort.py
@@ -1,63 +1,42 @@
+
+import time
 alist=[]
+start=time.time()
 def merge_sort(alist):
     if len(alist)>1:
         mid=len(alist)//2
         lefthalf=alist[:mid]
         righthalf=alist[mid:]
-    print(mid)
-    print(lefthalf)
-    print(righthalf)
-    for i in range(1,len(lefthalf)):
-        for j in range(i):
-            if lefthalf[j]> lefthalf[j+1]:
-                temp=lefthalf[j]
-                lefthalf[j]=lefthalf[j+1]
-                lefthalf[j+1]=temp
-                i+=1
-    print(lefthalf)   
-    
-    for i  in range(1,len(righthalf)):
-        for j in range(i):
-            if righthalf[j] > righthalf[j+1]:
-                temp=righthalf[j]
-                righthalf[j]=righthalf[j+1]
-                righthalf[j+1]=temp
-    print(righthalf)
-    for i in range(1,len(alist)):
-        for j in range(i):
-            if alist[j]>alist[j+1]:
-                temp=alist[j]
-                alist[j]=alist[j+1]
-                alist[j+1]=temp
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+        print(mid)
+        print(lefthalf)
+        print(righthalf)
+        for i in range(1,len(lefthalf)):
+            for j in range(i):
+                if lefthalf[j]> lefthalf[j+1]:
+                    temp=lefthalf[j]
+                    lefthalf[j]=lefthalf[j+1]
+                    lefthalf[j+1]=temp
+                    i+=1
+        print(lefthalf)   
+     
+        for i  in range(1,len(righthalf)):
+            for j in range(i):
+                if righthalf[j] > righthalf[j+1]:
+                    temp=righthalf[j]
+                    righthalf[j]=righthalf[j+1]
+                    righthalf[j+1]=temp
+        print(righthalf)
+        for i in range(1,len(alist)):
+            for j in range(i):
+
+                if alist[j]>alist[j+1]:
+                    temp=alist[j]
+                    alist[j]=alist[j+1]
+                    alist[j+1]=temp
     print(alist)
 merge_sort([1,13,11,100,52,89,98])
-alist=[]
-# def mergesort(alist):
-#     if len(alist)>1:
-#         mid=len(alist)//2
-#         lefthalf=alist[:mid]
-#         righthalf=alist[mid:]
-
-#         i=0
-#         j=0
-#         k=0
-#         while i<len(lefthalf) and j<len(righthalf):
-#             if lefthalf[i]<righthalf[j]:
-#                 alist[k] =lefthalf[i]
-#                 i+=1
-#             else:
-#                 alist[k]=righthalf[j]
-#                 j+=1
-#             k=k+1
-#         while i<len(lefthalf):
-#             alist[k]=lefthalf[i]
-#             i=i+1
-#             k=k+1
-#         while j< len(righthalf):
-#             alist[k]=righthalf[j]
-#             j=j+1
-#             k=k+1
-#     print("Merging",alist)
-# alist=[54,26,97,8,98]
-# mergesort(alist)
-# print(alist)
+end=time.time()
+t1=end-start
+print(t1*10000)
