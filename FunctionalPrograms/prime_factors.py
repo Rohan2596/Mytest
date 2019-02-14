76264931082673a32d558d967a8fbc41b84cc284
Rohan2596 <rohankadam662>
2019-02-14 17:14:25 +0530

    Python

--- FunctionalPrograms/prime_factors.py
+++ FunctionalPrograms/prime_factors.py
@@ -17,6 +17,7 @@ for num in range(0,num + 1):
 	if num > 1:
 		for i in range(2,num):
 			if (num % i) == 0:
+				
 				break
 			else:
 				print(num)
---
+++ FunctionalPrograms/tictactoe.py
@@ -0,0 +1,25 @@
+lst=[' ',' ',' ',' ',' ',' ',' ',' ']
+print(lst)
+# i=int(input("enter i"))
+# j=int(input("enter j"))
+# print(lst[i][j]="x")
+player=int(input("Enter the Player 1 or 2 :"))
+if player==1:
+	print("player one")
+elif player ==0:
+	print("player two")
+else:
+	print("Player not")
+i=0
+
+while i<8:
+	pos=int(input("enter i"))
+	
+	if lst[pos]==' ':
+		i+=1
+	lst[pos]="X"
+	print(lst)
+	
+# if ((lst[0][0]=="X" and lst[0][1]=="X" and lst[0][2]=="X") 
+# 	or (lst[1][0]=="X" and lst[1][1]=="X" and lst[1][2]=="X")
+# 	or (lst[0][0]=="X" and lst[0][1]=="X" and lst[0][2]=="X")):print("Player won")
